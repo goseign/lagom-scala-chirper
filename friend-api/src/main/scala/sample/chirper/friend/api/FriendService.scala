fbb11052c39890e839c1b5862bcbae638b587fa0
Nico Serfontein <nserfontein@gmail.com>
2017-11-07 06:26:49 +0200

    Activity stream service (#8)
    * Activity stream service
    
    * Activity stream service
    
    * Activity stream service
    
    * Activity stream service
    
    * Activity stream service

--- friend-api/src/main/scala/sample/chirper/friend/api/FriendService.scala
+++ friend-api/src/main/scala/sample/chirper/friend/api/FriendService.scala
@@ -1,9 +1,12 @@
 package sample.chirper.friend.api
 
 import akka.NotUsed
+import com.lightbend.lagom.scaladsl.api.Service._
 import com.lightbend.lagom.scaladsl.api.broker.Topic
 import com.lightbend.lagom.scaladsl.api.broker.kafka.{KafkaProperties, PartitionKeyStrategy}
 import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
+
+import scala.collection.immutable.Seq
 
 trait FriendService extends Service {
 
@@ -17,23 +20,18 @@ trait FriendService extends Service {
 
   def friendsTopic(): Topic[FriendAdded]
 
-  override def descriptor = {
-    import Service._
-
-    named("friendservice").withCalls(
-      pathCall("/api/users/:userId", getUser _),
-      namedCall("/api/users", createUser _),
-      pathCall("/api/users/:userId/friends", addFriend _),
-      pathCall("/api/users/:userId/followers", getFollowers _)
-    ).withTopics(
-      topic(FriendService.TopicName, friendsTopic _)
-        .addProperty(
-          KafkaProperties.partitionKeyStrategy,
-          PartitionKeyStrategy[FriendAdded](_.userId)
-        )
-    ).withAutoAcl(true)
-
-  }
+  override def descriptor = named("friendservice").withCalls(
+    pathCall("/api/users/:userId", getUser _),
+    namedCall("/api/users", createUser _),
+    pathCall("/api/users/:userId/friends", addFriend _),
+    pathCall("/api/users/:userId/followers", getFollowers _)
+  ).withTopics(
+    topic(FriendService.TopicName, friendsTopic _)
+      .addProperty(
+        KafkaProperties.partitionKeyStrategy,
+        PartitionKeyStrategy[FriendAdded](_.userId)
+      )
+  ).withAutoAcl(true)
 }
 
 object FriendService {
